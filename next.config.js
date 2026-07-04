44d24a6857dd2ef909db10dc01fbeea4898a4ccd
Giang Hoang Van <hvgiang86@gmail.com>
2026-07-04 18:49:49 +0700

    Make basePath dynamic for local dev and Netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+// detect if we are building in GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const basePath = isGithubActions ? '/cuacun2anni' : '';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/cuacun2anni',
+  basePath: basePath,
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/cuacun2anni',
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
   images: {
     unoptimized: true,
